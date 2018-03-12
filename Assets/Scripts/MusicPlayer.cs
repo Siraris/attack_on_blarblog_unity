afa3482a9bcd9b98ec3e524ac5a463d777437998
Andrew Soep <siraris@gmail.com>
2018-03-11 22:11:51 -0500

    Initial commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private AudioSource _splashAudio;
+	// Use this for initialization
+
+	void Awake() {
+		DontDestroyOnLoad(this);
+	}
+
+	void Start () {
+		_splashAudio = GetComponent<AudioSource>();
+		_splashAudio.Play();
+		Invoke("LoadFirstLevel", 8f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void LoadFirstLevel() {
+		SceneManager.LoadScene("Level 1");
+	}
+}
